1884df4c6ed2a61a6bff80a0a165cb44576ed169
dgmolina <dgmolina.ti@gmail.com>
2019-03-21 10:55:40 -0300

    incluindo divisao()

--- teste.py
+++ teste.py
@@ -1,8 +1,16 @@
 import pytest
-from utilidades import multiplicacao
+from utilidades import multiplicacao, divisao
+
 
 def teste_mult():
     assert multiplicacao(2,2) == 4
     assert multiplicacao(2,0) == 0
     assert multiplicacao(-1, 20) == -20
     assert multiplicacao(3,1.5) == 4.5
+
+def teste_div():
+    assert divisao(4,2) == 2
+    assert divisao(5,2) == 2.5
+    assert divisao(0, 10) == 0
+    assert divisao(-10, 2) == -5
+    assert divisao(10, -2) == -5 
--- utilidades.py
+++ utilidades.py
@@ -1,2 +1,5 @@
 def multiplicacao(a, b):
     return a * b
+
+def divisao(a, b):
+    return float(a) / b
